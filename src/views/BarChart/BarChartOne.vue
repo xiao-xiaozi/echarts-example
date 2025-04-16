311dc07e339118c1f11c668e855bd12e64d88776
xiao-xiaozi <xiao-xiaozi@outlook.com>
2025-04-16 10:50:25 +0800

    feat: 调整柱状图顺序

--- src/views/BarChart/BarChartOne.vue
+++ src/views/BarChart/BarChartOne.vue
@@ -74,106 +74,83 @@ function initBarChartOne(){
 
 function initBarChartTwo(){
   const barTwo = echarts.init(document.getElementById('bar-two'))
+
   barTwo.setOption({
     tooltip: {
       trigger: 'axis',
       axisPointer: { type: 'shadow' }
     },
     grid: {
-      right: '25%',
+      left: '10%',
+      right: '4%',
+      bottom: '3%',
       containLabel: true
     },
-    dataZoom: [
-      {
-        type: 'slider',
-        right: 70,
-        yAxisIndex: 0,
-        startValue: 0,
-        endValue: 2,
-        showDetail: false,
-        showDataShadow: false,
-        brushSelect: false,
-        zoomLock: true, // 锁定选择区域大小，只能平移，不能缩放
-        filter: 'empty',
-        fillerColor: '#335759',
-        borderColor: 'transparent',
-        width: 5,
-        handleSize: '90%',
-        backgroundColor: '#eee',
-        handleIcon: 'path://M512,512m-448,0a448,448,0,1,0,896,0a448,448,0,1,0,-896,0Z',
-        handleStyle: {
-          color: '#335758',
-          borderWidth: 0
-        }
-      }
-    ],
-    xAxis: {
-      type: 'value',
-      boundaryGap: [0, 0.01]
-    },
-    yAxis: [
+    xAxis: [
       {
         type: 'category',
-        data: ['Brazil', 'Indonesia', 'USA', 'India', 'China', 'World']
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+        // axisTick: { alignWithLabel: true }
       }
+    ],
+    yAxis: [
+      { type: 'value' }
     ],
     series: [
       {
-        name: '2011',
+        name: 'Direct',
         type: 'bar',
-        barGap: '60%',
-        barWidth: 30,
-        label: {
-          show: true,
-          position: 'right',
-          offset: [100, 0],
-          formatter: ({ data }) => {
-            return `{a|${data.value}} {b|${data.percent}}`
-          },
-          rich: {
-            a: { color: '#000' },
-            b: {
-              color: '#FFCA55',
-              padding: 10,
-              borderRadius: 10,
-              backgroundColor: 'rgba(255,202,85,0.10)'
-            }
-          }
-        },
-        data: [
-          {
-            name: 'Brazil',
-            value: 18203,
-            percent: 0.34
-          },
-          {
-            name: 'Indonesia',
-            value: 18203,
-            percent: 0.34
-          },
-          {
-            name: 'USA',
-            value: 18203,
-            percent: 0.34
-          },
-          {
-            name: 'India',
-            value: 18203,
-            percent: 0.34
-          },
-          {
-            name: 'China',
-            value: 18203,
-            percent: 0.34
-          },
-          {
-            name: 'World',
-            value: 18203,
-            percent: 0.34
-          }
-        ]
+        barWidth: '60%',
+        data: [10, 52, 200, 334, 390, 330, 220],
+        markArea: {
+          silent: true,
+          label: { position: 'insideTop',color: 'red' },
+          data: [
+            [
+              {
+                name: '测试label',
+                xAxis: 'Mon',
+                itemStyle: {
+                  color: {
+                    type: 'linear',
+                    x: 0,
+                    y: 0,
+                    x2: 0,
+                    y2: 1,
+                    colorStops: [
+                      { offset: 0,color: "rgba(58,175,255,0.1)" },
+                      { offset: 1,color: "rgba(58,175,255,0)" }
+                    ]
+                  }
+                }
+              },
+              { xAxis: 'Thu' }
+            ],
+            [
+              {
+                name: '测试label_2',
+                xAxis: 'Fri',
+                itemStyle: {
+                  color: {
+                    type: 'linear',
+                    x: 0,
+                    y: 0,
+                    x2: 0,
+                    y2: 1,
+                    colorStops: [
+                      { offset: 0,color: "rgba(253,131,54,0.1)" },
+                      { offset: 1,color: "rgba(253,131,54,0)" }
+                    ]
+                  }
+                }
+              },
+              { xAxis: 'Sun' }
+            ]
+          ]
+        }
       }
-    ]
+    ],
+
   })
 }
 
@@ -203,9 +180,7 @@ function nextPage() {
       </div>
       <div class="fourth-box">
         <ul>
-          <li>通过数据缩放(dataZoom)设置dataZoom.zoomLock锁定选择区域、实现平移（类滚动）效果。 </li>
-          <li>平移（类滚动）效果，需要关闭调刷选功能，dataZoom.brushSelect设置为false。</li>
-          <li>通过设置label的formatter和rich属性，实现label多段文本且不同样式呈现。</li>
+          <li>通过 <a href="https://echarts.apache.org/zh/option.html#series-line.markArea" target="_blank">markArea</a> 实现坐标系区域高亮。</li>
         </ul>
       </div>
     </main>
